3b864cf89cfc8d3bbeba8fe60a8cc20b3896eded
MohamedAzmy0 <112773269+MohamedAzmy0@users.noreply.github.com>
2022-09-24 20:15:27 +0200

    Add files via upload

---
+++ About.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd 
+
+st.title("About")
+
+st.info("This is a test")
+
+st.success("This is a test")
+
+name=st.text_input("Enter your name")
+
+age=st.number_input("Enter your age")
+
+height=st.slider("Enter your height",min_value=0,max_value=200 ,value=100 ,step=1)
+
+
+def get_data():
+    return pd.DataFrame({'name' : [name],
+                         'age' : [age],
+                         'height' : [height]})
---
+++ Data.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+st.title("Data")
+
+st.write("This is a test")
