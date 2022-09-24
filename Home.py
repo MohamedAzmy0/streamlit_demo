c19c3869c26cb337bc376e9db2c63bd5b6ebbdcd
MohamedAzmy0 <112773269+MohamedAzmy0@users.noreply.github.com>
2022-09-24 20:15:12 +0200

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("Hello World") 
+
+st.write("This is a test")
+
+st.markdown(''' 
+                * This is a test
+                * This is a test
+                * This is a test    ''')
+
+
+st.markdown(''' 
+                1. This is a test
+                2. This is a test
+                3. This is a test    ''')
